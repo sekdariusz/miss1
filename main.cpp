a2750aa7904acb5192e5a0ebd23d043b10d2ad60
Darek <sekdariusz@gmail.com>
2015-10-06 20:45:19 +0200

    Dodane komentarze i niedziałające zadanie 3

--- main.cpp
+++ main.cpp
@@ -28,11 +28,17 @@ int main(int argc, char** argv) {
     //srednia
     mpf_t avg;
     
-    //srednia
+    //wariancja
     mpf_t var;
     
+    //pomocnicza do 3 zad
+    mpf_t help;
+    
+    //liczik do 3 zad
+    mpf_t counter;
+    
     //inicjacja
-    mpf_inits(num, sum, amount, avg, var);
+    mpf_inits(num, sum, amount, avg, var, help, counter);
     
     //zad 1
     file = fopen("test1", "r");
@@ -45,39 +51,82 @@ int main(int argc, char** argv) {
     char str[1024];
     while (!feof (file))
     {  
+      //pobieram kolejna liczbę z pliku
       fscanf (file, "%s", str);
+      
+      //zamieniam na tym gmp
       mpf_set_str(num, str, 10);
       
+      //dodaje liczbe do sumy
       mpf_add(sum, sum, num);
+      
+      //zwiększam licznik liczb o 1
       mpf_add_ui(amount, amount, 1);
     }
-        
+    
+    //dzielenie: suma_liczb/ilość_liczb = wartość średnia
     mpf_div(avg, sum, amount);
     
+    //wyświetlanie
     gmp_printf ("Ilosc liczb %.Ff\n", amount, 50);
     gmp_printf ("Suma liczb %.Ff\n", sum, 50);
     gmp_printf ("Wartosc srednia  %.Ff\n", avg, 50);
     
     //zad 2
+    
+    //średnia wyliczona wcześniej do kwadratu
     mpf_pow_ui(avg, avg, 2);
     
+    //przewijam plik do początku
     rewind(file);
     while (!feof (file))
     {  
+      //pobieram kolejna liczbę z pliku
       fscanf (file, "%s", str);
+      
+      //zamieniam na typ gmp
       mpf_set_str(num, str, 10);
+      
+      //podnoszę liczbę do drugiej potęgi
       mpf_pow_ui(num, num, 2);
+      
+      //odejmuję od kwadratu obecnej liczby kwadrat średniej
       mpf_sub(num, num, avg);
+      
+      //dodaję poprzedni wynik do sumy
       mpf_add(var, var, num);
     }
+    
+    //dzielenie: suma/ilość_liczb
     mpf_div(var, var, amount);
     gmp_printf ("Wariancja  %.Ff\n", var, 50);
 
     //zad 3
     rewind(file);
 
+    fscanf (file, "%s", str);
+    mpf_set_str(num, str, 10);
+    
+    while(!feof (file))
+    {
+     //zwiększam licznik o 1
+     mpf_add_ui(counter, counter, 1);
 
+     //pobieram kolejną liczbę z pliku
+     fscanf (file, "%s", str);
+     
+     //zamieniam na typ gmp
+     mpf_set_str(help, str, 10);
+     
+     //sprawdzam czy liczby są sobie równe, jeśli tak to kończę while
+     if(mpf_cmp(num, help)) {
+         break;
+     }
+    }
+    
     fclose (file);    
+    
+    gmp_printf ("Okres  %.Ff\n", counter, 50);
     
     //mpf_clears (amount, sum, num, avg);
     
--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=CppApplication_1
+PROJECTNAME=miss1
 
 # Active Configuration
 DEFAULTCONF=Debug
